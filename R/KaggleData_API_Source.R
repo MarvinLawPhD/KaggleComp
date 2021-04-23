40f2763e1c78fbc9211d18b24efc6140f81e5455
mlaw4494 <mlaw4494@uni.sydney.edu.au>
2021-04-23 16:45:02 +1000

    R API Source script for Kaggle datasets

---
+++ R/KaggleData_API_Source.R
@@ -0,0 +1,59 @@
+
+# Script used to source the data for Kaggle Competitions using API
+# Packages used:
+
+library(tidyverse)
+library(httr)
+library(jsonlite)
+
+# Read API Key via JSON file
+
+apikey <- rjson::fromJSON(file = "kaggle.json")
+
+apibaseurl <- "https://www.kaggle.com/"
+
+# Examine the competition list datasets (page 1)
+
+path <- "api/v1/competitions/list"
+
+kaggle_get <- GET(url = apibaseurl, path = path, authenticate(apikey$username, apikey$key))
+
+
+kaggle_get$status_code
+
+kaggle_competition_list <- kaggle_get$content %>% 
+  rawToChar() %>% 
+  fromJSON() %>% 
+  as_tibble()
+
+# Titanic
+
+# Find data files attached to Titanic Competition (5th dataset)
+
+data_path <- paste0("api/v1/competitions/data/list/", kaggle_competition_list$id[5])
+
+data_get <- GET(url = apibaseurl, path = data_path, authenticate(apikey$username, apikey$key))
+
+data_get$status_code
+
+data_get$content %>% rawToChar() %>% fromJSON()
+
+# Download data files
+
+testdata_path <- paste0("api/v1/competitions/data/download/", kaggle_competition_list$id[5], "/test.csv")
+traindata_path <- paste0("api/v1/competitions/data/download/", kaggle_competition_list$id[5], "/train.csv")
+genderdata_path <- paste0("api/v1/competitions/data/download/", kaggle_competition_list$id[5], "/gender_submission.csv")
+
+titanic_get_test <- GET(url = apibaseurl, path = testdata_path, authenticate(apikey$username, apikey$key))
+titanic_get_train <- GET(url = apibaseurl, path = traindata_path, authenticate(apikey$username, apikey$key))
+titanic_get_gender <- GET(url = apibaseurl, path = genderdata_path, authenticate(apikey$username, apikey$key))
+
+titanic_data_test <- titanic_get_test$content %>%
+  rawToChar() %>% read_csv()
+
+titanic_data_train <- titanicget_test$content %>%
+  rawToChar() %>% read_csv()
+
+titanic_data_gender <- titanic_get_test$content %>%
+  rawToChar() %>% read_csv()
+
