0cc96aced89aaf05a2f1f8245ec8a086342b3a31
mlaw4494 <mlaw4494@uni.sydney.edu.au>
2021-04-28 17:22:48 +1000

    NLP kaggle comp

--- R/KaggleData_API_Source.R
+++ R/KaggleData_API_Source.R
@@ -104,3 +104,106 @@ train_data %>%
 train_data_V2 <- train_data %>% 
   mutate(Child = Age < 18)
 
+# Natural Language Processing for Disaster Tweets
+
+nlp_test <- read_csv("R/NLP/test.csv")
+nlp_train <- read_csv("R/NLP/train.csv")
+
+# Find out how many words in each text
+
+library(tidytext)
+library(randomForest)
+library(corrr)
+
+
+data(stop_words)
+
+library("syuzhet")
+
+nlp_train_split <- nlp_train %>% 
+  unnest_tokens(word, text) %>% 
+  # anti_join(stop_words) %>% 
+  mutate(word2 = str_extract(word, "[a-z']+"))
+
+nlp_train_sentiment <- nlp_train_split %>% 
+  mutate(sentiment = get_sentiment(word, method="syuzhet")) %>% 
+  group_by(id) %>% 
+  summarise(sentiment = mean(sentiment))
+
+nlp_train_freq <- nlp_train_split %>% 
+  mutate(word2 = str_extract(word, "[a-z']+")) %>%
+  group_by(word2) %>% 
+  summarise(freq = n())
+
+nlp_train_wordlength <- nlp_train_split %>% 
+  mutate(wordlength = nchar(word)) %>% 
+  group_by(id) %>% 
+  summarise(meanwl = mean(wordlength),
+            minwl = min(wordlength),
+            maxwml = max(wordlength))
+
+
+nlp_train_fe <- nlp_train_split %>% 
+  left_join(nlp_train_freq, by = "word2") %>%
+  group_by(id, target) %>% 
+  summarise(nwords = n(),
+            freqavg = mean(freq)) %>% 
+  left_join(nlp_train_wordlength, by = "id") %>% 
+  left_join(nlp_train_sentiment, by = "id") %>%
+  ungroup() %>% 
+  mutate(target = as.factor(target))
+  
+set.seed(123)
+
+nlp_train_fe1 <- nlp_train_fe %>% ungroup() %>% slice_sample(n = round(.75*nrow(nlp_train_fe)))
+nlp_train_fe2 <- anti_join(nlp_train_fe, nlp_train_fe1, by = 'id')
+
+
+nlp_rf <- randomForest(target ~ ., data=nlp_train_fe1 %>% select(-id), 
+                       ntree=100, mtry=2, importance=TRUE)
+
+varImpPlot(nlp_rf)
+
+nlp_rf_predict <- predict(nlp_rf,nlp_train_fe2)
+
+
+table(nlp_rf_predict,nlp_train_fe2$target)
+
+nlp_test_split <- nlp_test %>% 
+  unnest_tokens(word, text) %>% 
+  mutate(word2 = str_extract(word, "[a-z']+"))
+
+nlp_test_sentiment <- nlp_test_split %>% 
+  mutate(sentiment = get_sentiment(word, method="syuzhet")) %>% 
+  group_by(id) %>% 
+  summarise(sentiment = mean(sentiment))
+
+nlp_test_freq <- nlp_test_split %>% 
+  mutate(word2 = str_extract(word, "[a-z']+")) %>%
+  group_by(word2) %>% 
+  summarise(freq = n())
+
+nlp_test_wordlength <- nlp_test_split %>% 
+  mutate(wordlength = nchar(word)) %>% 
+  group_by(id) %>% 
+  summarise(meanwl = mean(wordlength),
+            minwl = min(wordlength),
+            maxwml = max(wordlength))
+
+nlp_test_fe <- nlp_test_split %>% 
+  left_join(nlp_test_freq, by = "word2") %>%
+  group_by(id) %>% 
+  summarise(nwords = n(),
+            freqavg = mean(freq)) %>% 
+  left_join(nlp_test_wordlength, by = "id") %>% 
+  left_join(nlp_test_sentiment, by = "id") %>%
+  ungroup()
+
+nlp_rf_predicttest <- predict(nlp_rf,nlp_test_fe)
+
+nlp_test$target <- nlp_rf_predicttest
+
+nlp_test %>% 
+  select(id, target) %>% 
+  write.csv("R/NLP/submission.csv", row.names = FALSE)
+
